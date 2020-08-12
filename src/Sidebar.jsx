b05855875da8e4c6c8cd08476f4083ac64dab833
yomna raouf <yomnaraouf99@gmail.com>
2020-08-12 10:01:25 -0700

    sidebar done & connection to db done

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
+import SidebarOption from './SidebarOption';
+
 import './Sidebar.css';
+
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecordRounded';
 import CreateIcon from '@material-ui/icons/Create';
-import SidebarOption from './SidebarOption';
 import InsertCommentIcon from '@material-ui/icons/InsertCommentRounded';
+import InboxIcon from '@material-ui/icons/InboxRounded';
+import DraftsIcon from '@material-ui/icons/DraftsRounded';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorderRounded';
+import PeopleAltIcon from '@material-ui/icons/PeopleAltRounded';
+import AppsIcon from '@material-ui/icons/AppsRounded';
+import FileCopyIcon from '@material-ui/icons/FileCopyRounded';
+import ExpandLessIcon from '@material-ui/icons/ExpandLessRounded';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMoreRounded';
+import AddIcon from '@material-ui/icons/Add';
+
+import db from './firebase';
 
 function Sidebar() {
+
+    const [channels, setChannels] = useState([]);
+
+    useEffect( () => {
+        db.collection('rooms').onSnapshot( snapshot => (
+            setChannels(snapshot.docs.map( doc => ({
+                    id:doc.id,
+                    name: doc.data().name
+                }))
+            )
+        ));
+    }, []);
+
     return (
         <div className='sidebar'>
             <div className="sidebar__header">
@@ -19,8 +46,22 @@ function Sidebar() {
                 <CreateIcon />
             </div>
             <SidebarOption Icon={InsertCommentIcon} title={'Threads'} />
-            <SidebarOption  title={'Threads'} />
-            
+            <SidebarOption Icon= {InboxIcon}  title={'Mentions & Reactions'} />
+            <SidebarOption Icon= {DraftsIcon}  title={'Saved items'} />
+            <SidebarOption Icon= {BookmarkBorderIcon}  title={'channel browser'} />
+            <SidebarOption Icon= {PeopleAltIcon}  title={'People & user groups'} />
+            <SidebarOption Icon= {AppsIcon}  title={'Apps'} />
+            <SidebarOption Icon= {FileCopyIcon}  title={'File browser'} />
+            <SidebarOption Icon= {ExpandLessIcon}  title={'Show less'} />
+            <hr />
+            <SidebarOption Icon= {ExpandMoreIcon}  title={'Channels'} />
+            <hr />
+            <SidebarOption Icon= {AddIcon}  title={'Add channel'} />
+
+            {channels.map( channel => (
+                <SidebarOption key={channel.id}  title={channel.name}  />
+            ))}
+
             
         </div>
     )
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "slack-clone-5b2ee.firebaseapp.com",
+    databaseURL: "https://slack-clone-5b2ee.firebaseio.com",
+    projectId: "slack-clone-5b2ee",
+    storageBucket: "slack-clone-5b2ee.appspot.com",
+    messagingSenderId: "760320379129",
+    appId: "1:760320379129:web:ed81b8360ae9a40d217a28",
+    measurementId: "G-LDHL1G9X8Z"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
